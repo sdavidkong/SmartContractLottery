dedd373723aa96b418c94739abeb1fd6685d5b67
S David Kong <SDAVIDKONG@gmail.com>
2022-09-21 13:16:06 -0400

    added functions to Raffle

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract Raffle {
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
+error Raffle__NotEnoughEthSent();
+
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private i_entranceFee;
     address payable[] private s_players;
 
-    error Raffle__NotEnoughEthSent();
+    event RaffleEnter(address indexed player);
 
-    constructor(uint256 entranceFee) {
+    constructor(address vrfCoordinatorV2, uint256 entranceFee)
+        VRFConsumerBaseV2(vrfCoordinatorV2)
+    {
         i_entranceFee = entranceFee;
     }
 
@@ -15,7 +21,16 @@ contract Raffle {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughEthSent();
         }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
     }
+
+    function pickRandomWinner() external {}
+
+    function fulfillRandomWords(uint256 requestID, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
