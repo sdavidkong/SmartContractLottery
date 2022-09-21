1aa0a16eeae3e5b174e8d25732a08218e8306b8d
S David Kong <SDAVIDKONG@gmail.com>
2022-09-21 17:40:43 -0400

    raffle update

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,19 +2,29 @@
 pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Raffle__NotEnoughEthSent();
 
 contract Raffle is VRFConsumerBaseV2 {
-    uint256 private i_entranceFee;
+    uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
 
     event RaffleEnter(address indexed player);
 
-    constructor(address vrfCoordinatorV2, uint256 entranceFee)
-        VRFConsumerBaseV2(vrfCoordinatorV2)
-    {
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
     }
 
     function enterRaffle() public payable {
@@ -25,7 +35,15 @@ contract Raffle is VRFConsumerBaseV2 {
         emit RaffleEnter(msg.sender);
     }
 
-    function pickRandomWinner() external {}
+    function pickRandomWinner() external {
+        i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        )
+    }
 
     function fulfillRandomWords(uint256 requestID, uint256[] memory randomWords)
         internal
