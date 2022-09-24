412cc4ffa2b033c29ea366075ed5590aaa12cdaf
S David Kong <SDAVIDKONG@gmail.com>
2022-09-24 11:32:12 -0400

    deploy script updates

--- scripts/01-deploy.js
+++ scripts/01-deploy.js
@@ -1,8 +1,12 @@
 const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
 
 module.exports = async function({ getNamedAccounts, deployments }) {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+
+  if (developmentChains.includes(network.name)) {
+  }
 
   const raffle = await deploy("Raffle", {
     from: deployer,
